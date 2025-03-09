71d58185736dfea02a2650c8ed17f69102e44c3b
Lada Egorova <ladaegorova2899@gmail.com>
2025-03-09 12:53:28 +0100

    Added first gif and onClick() handler

---
+++ script.js
@@ -0,0 +1,12 @@
+// let img = document.createElement("img");
+// img.src = "gif_image_cat.gif";
+// img.alt = "An Example";
+// img.width = 400;
+// img.height = 300;
+// document.getElementById("gif_cat").appendChild(img);
+
+function onclick_event() {
+     document.getElementById("gif_cat").innerHTML = "Welcome to GeeksforGeeks!";
+}
+
+// https://www.geeksforgeeks.org/how-to-add-gif-in-html/

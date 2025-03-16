0b4045bb7583e3b84499487386ee32bc654b22b9
Lada Egorova <ladaegorova2899@gmail.com>
2025-03-16 17:26:55 +0100

    Reorganized files structure

--- script.js
+++ script.js
@@ -5,8 +5,22 @@
 // img.height = 300;
 // document.getElementById("gif_cat").appendChild(img);
 
+// document.getElementById("gif_cat").addEventListener("mouseover", over);
+
 function onclick_event() {
      document.getElementById("gif_cat").innerHTML = "Welcome to GeeksforGeeks!";
 }
+
+ function over() {
+     document.getElementById("gif_cat").style.color = "green";
+ }
+
+ function openPDF() {
+    // URL of the PDF file
+    var pdfURL = 'source/CV_Egorova.pdf';
+  
+    // Open the PDF in a new tab
+    window.open(pdfURL, '_blank');
+  }
 
 // https://www.geeksforgeeks.org/how-to-add-gif-in-html/
